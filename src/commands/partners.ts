200cc48ee1e439e74e10ea16ce2b8b17bf8fe1cf
neptunian <hazmaker@proton.me>
2024-07-14 22:22:14 -0500

    Update partners.ts

--- src/commands/partners.ts
+++ src/commands/partners.ts
@@ -1,1 +1,14 @@
-
+const partnersObj = {
+  "message" : [
+    [
+      "Partners:",
+      " "
+    ],
+    [
+      "737 happily has allied the following groups:",
+      " ",
+      "Cre.ep",
+      "2226"
+    ],
+  ]
+}
